aa04029ee96f6f0b61d138bc9a55869c9ea44fea
Priyanshu Lanjewar <priyanshu@lanjewar.in>
2025-05-09 08:59:40 +0530

    Update resume.js

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,5 +1,19 @@
-var url = 'https://github.com/priyanshu-lanjewar/resume/releases/download/v2.0.0/Priyanshu_Lanjewar.pdf';
 var resumeBtn = document.getElementById('resume-btn');
-resumeBtn.onclick = function() {
-    location.assign(url);
-}
+var baseUrl = 'https://github.com/priyanshu-lanjewar/resume/releases/download/';
+
+fetch('https://api.github.com/repos/priyanshu-lanjewar/resume/releases/latest')
+  .then(response => response.json())
+  .then(data => {
+    var latestTag = data.tag_name; // e.g., "v2.0.0"
+    var fileName = 'Priyanshu_Lanjewar.pdf';
+    var downloadUrl = baseUrl + latestTag + '/' + fileName;
+
+    resumeBtn.onclick = function() {
+      location.assign(downloadUrl);
+    };
+  })
+  .catch(error => {
+    console.error('Error fetching latest release:', error);
+    resumeBtn.disabled = true;
+    resumeBtn.title = 'Resume unavailable';
+  });
